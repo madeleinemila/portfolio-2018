ffc9ca9141bbb19d08f74ce51ca537cbf9ad4ffc
Madeleine <32352098+madeleinemila@users.noreply.github.com>
2017-12-25 18:30:19 +1100

    writing and retrieving from db

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,7 @@
 class PagesController < ApplicationController
   def home
-    @response = HTTParty.get('http://api.whatpulse.org/user.php?user=itsmadd&format=json')
-    @data = Keystroke.get_data
-    @pulse_date = @data['pulse_date']
-    @total = @data['total']
-    @average = @data['average']
+    # @response = HTTParty.get('http://api.whatpulse.org/user.php?user=itsmadd&format=json')
+    Keystroke.update_data
+    @data = Keystroke.last
   end
 end
--- app/models/keystroke.rb
+++ app/models/keystroke.rb
@@ -11,18 +11,25 @@
 #
 
 class Keystroke < ApplicationRecord
-  def self.get_data
-    @data = {}
+  def self.update_data
+    # @data = {}
     @response = HTTParty.get('http://api.whatpulse.org/user.php?user=itsmadd&format=json')
     if @response.success?
-      puts '**** Retrieved keystroke data ****'
-      k = Keystroke.new
-      k.pulse_date = @response['LastPulse']
-      k.total = @response['Keys']
-      if k.save
-        puts '++++ Database updated ++++'
+      prev_date = Keystroke.last.pulse_date.strftime('%Y-%m-%d')
+      new_date = @response['LastPulse'].slice(0, 10)
+      # IF THIS IS NEW DATA
+      if new_date != prev_date
+        puts '**** Retrieved keystroke data ****'
+        k = Keystroke.new
+        k.pulse_date = new_date
+        k.total = @response['Keys']
+        if k.save
+          puts '++++ Database updated ++++'
+        end
+      else
+        puts 'xxxx Retrieved keystroke data - date already exists in database xxxx'
       end
     end
-    @data
+    # @data
   end
 end
