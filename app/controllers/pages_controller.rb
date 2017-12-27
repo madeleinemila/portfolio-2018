9ee624dadd4bc46221953fd92a6a90632c582962
Madeleine <32352098+madeleinemila@users.noreply.github.com>
2017-12-27 20:19:55 +1100

    erb plus p5

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,14 +5,9 @@ class PagesController < ApplicationController
 
     @avg_all = Keystroke.calc_avg_all
 
-    @avg_prev = Keystroke.calc_avg_prev 1
-    @avg_prev_2 = Keystroke.calc_avg_prev 2
-    @avg_prev_3 = Keystroke.calc_avg_prev 3
-
     # first prev = 2719.004917558577
     # second prev = 2091
     # third = 18240
-
 
     @avg_prev = []
     Keystroke.all.each_with_index do |k, i|
