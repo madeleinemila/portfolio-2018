b6c9d64c6483d19f37b0303c445fbafcfdb4c655
Madeleine <32352098+madeleinemila@users.noreply.github.com>
2017-12-30 20:04:57 +1100

    started styling about and work

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def home
-    Keystroke.update_data
+    # Keystroke.update_data
     @avg_all = Keystroke.calc_avg_all
     @avg_prev = Keystroke.calc_avg_prev_array
   end
