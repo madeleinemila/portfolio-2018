0ceac1015d2ff3227266d4243bf5be6e4d5d62d3
Madeleine <32352098+madeleinemila@users.noreply.github.com>
2018-01-02 22:10:26 +1100

    text fades with temp text

--- app/assets/javascripts/rotate-text.js
+++ app/assets/javascripts/rotate-text.js
@@ -1,6 +1,9 @@
 // *** 'from' text options
 const from = ['film', 'tape', 'hardware'];
 
+// ** 'to' text options - keep same length as 'from' // TODO put in one 2d array then?
+const to = ['web delivery', 'web app', 'DMX over IP'];
+
 // counter to rotate through options
 let counter = 1;
 
@@ -13,7 +16,7 @@ const fadeOut = function(elem) {
 };
 
 const changeText = function(elem, a) {
-  console.log(counter);
+  // console.log(counter);
   elem.text( a[counter] );
   counter++;
   if (counter > from.length - 1) counter = 0 ;
@@ -23,8 +26,7 @@ const fadeIn = function(elem) {
   elem.removeClass('no-opacity');
 };
 
-
-
+// ** MAIN FADE LOOP **
 const fadeLoop = function( $elem, a ) {
   setTimeout(() => { fadeOut( $elem ) }, 2000 );
   setTimeout(() => { changeText( $elem, a ) }, 3000 );
@@ -37,16 +39,12 @@ const fadeLoop = function( $elem, a ) {
 
 
 
-
-
-
-
-
-
 $(document).ready(function() {
+  // lhs 'from' text loop through
   const $rotateFrom = $('#rotateFrom');
   fadeLoop( $rotateFrom, from );
 
-
-
+  // rhs 'to' text loop through
+  const $rotateTo = $('#rotateTo');
+  fadeLoop( $rotateTo, to );
 });
