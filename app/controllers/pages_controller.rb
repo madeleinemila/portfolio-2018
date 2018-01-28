ad42c3bb0b06dfaaf4e1da938b76f95d0c85ae85
Madeleine <32352098+madeleinemila@users.noreply.github.com>
2018-01-28 23:51:46 +1100

    temp removed db query for keystrokes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,8 @@
 class PagesController < ApplicationController
   def home
     # Keystroke.update_data
-    @avg_all = Keystroke.calc_avg_all
-    @avg_prev = Keystroke.calc_avg_prev_array
+    # @avg_all = Keystroke.calc_avg_all
+    # @avg_prev = Keystroke.calc_avg_prev_array
   end
 
   def about
